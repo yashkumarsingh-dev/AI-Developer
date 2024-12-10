bc6bd4b91abf49dfbcded0893e0d50b1de698813
Ankur77720 <prajapatiankur8349@gmail.com>
2024-12-10 12:30:51 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import morgan from 'morgan';
+
+
+const app = express();
+
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+export default app; 
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config()
+import http from 'http';
+import app from './app.js';
+
+const port = process.env.PORT || 3000;
+
+
+
+const server = http.createServer(app);
+
+
+
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
