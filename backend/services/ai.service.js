9c9b1921ffd522fe7812b8a6b188deeea1cd207b
Ankur77720 <prajapatiankur8349@gmail.com>
2024-12-14 17:10:14 +0530

    add AI service integration; implement AI routes and controller for content generation

---
+++ backend/services/ai.service.js
@@ -0,0 +1,11 @@
+import { GoogleGenerativeAI } from "@google/generative-ai"
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+export const generateResult = async (prompt) => {
+
+    const result = await model.generateContent(prompt);
+
+    return result.response.text()
+}
