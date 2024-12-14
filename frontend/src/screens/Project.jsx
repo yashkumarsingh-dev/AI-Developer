fce999cadb2847be7311fb8d7682edc8ea43c86e
Ankur77720 <prajapatiankur8349@gmail.com>
2024-12-14 12:00:14 +0530

    fetch project details and users in Project component; implement user selection for collaborators

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
+import axios from '../config/axios'
 
 const Project = () => {
 
@@ -8,24 +9,63 @@ const Project = () => {
     const [ isSidePanelOpen, setIsSidePanelOpen ] = useState(false)
     const [ isModalOpen, setIsModalOpen ] = useState(false)
     const [ selectedUserId, setSelectedUserId ] = useState([])
+    const [ project, setProject ] = useState(location.state.project)
 
-    const users = [
-        { id: 1, name: 'User One' },
-        { id: 2, name: 'User Two' },
-        { id: 3, name: 'User Three' },
-        { id: 4, name: 'User Four' },
-        { id: 5, name: 'User Five' },
-        { id: 6, name: 'User Six' },
-        { id: 7, name: 'User Seven' },
-        { id: 8, name: 'User Eight' },
-        { id: 9, name: 'User Nine' },
-        { id: 10, name: 'User Ten' },
-    ]
+    const [ users, setUsers ] = useState([])
 
     const handleUserClick = (id) => {
-        setSelectedUserId([ ...selectedUserId, id ])
+        setSelectedUserId(prevSelectedUserId => {
+            const newSelectedUserId = new Set(prevSelectedUserId);
+            if (newSelectedUserId.has(id)) {
+                newSelectedUserId.delete(id);
+            } else {
+                newSelectedUserId.add(id);
+            }
+
+            return newSelectedUserId;
+        });
+
 
     }
+
+
+    function addCollaborators() {
+
+        axios.put("/projects/add-user", {
+            projectId: location.state.project._id,
+            users: Array.from(selectedUserId)
+        }).then(res => {
+            console.log(res.data)
+            setIsModalOpen(false)
+
+        }).catch(err => {
+            console.log(err)
+        })
+
+    }
+
+    useEffect(() => {
+
+        axios.get(`/projects/get-project/${location.state.project._id}`).then(res => {
+
+            console.log(res.data.project)
+
+            setProject(res.data.project)
+        })
+
+
+
+        axios.get('/users/all').then(res => {
+
+            setUsers(res.data.users)
+
+        }).catch(err => {
+
+            console.log(err)
+
+        })
+
+    }, [])
 
     return (
         <main className='h-screen w-screen flex'>
@@ -56,18 +96,32 @@ const Project = () => {
                     </div>
                 </div>
                 <div className={`sidePanel w-full h-full flex flex-col gap-2 bg-slate-50 absolute transition-all ${isSidePanelOpen ? 'translate-x-0' : '-translate-x-full'} top-0`}>
-                    <header className='flex justify-end px-4 p-2 bg-slate-200'>
+                    <header className='flex justify-between items-center px-4 p-2 bg-slate-200'>
+
+                        <h1
+                            className='font-semibold text-lg'
+                        >Collaborators</h1>
+
                         <button onClick={() => setIsSidePanelOpen(!isSidePanelOpen)} className='p-2'>
                             <i className="ri-close-fill"></i>
                         </button>
                     </header>
                     <div className="users flex flex-col gap-2">
-                        <div className="user cursor-pointer hover:bg-slate-200 p-2 flex gap-2 items-center">
-                            <div className='aspect-square rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'>
-                                <i className="ri-user-fill absolute"></i>
-                            </div>
-                            <h1 className='font-semibold text-lg'>username</h1>
-                        </div>
+
+                        {project.users && project.users.map(user => {
+
+
+                            return (
+                                <div className="user cursor-pointer hover:bg-slate-200 p-2 flex gap-2 items-center">
+                                    <div className='aspect-square rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'>
+                                        <i className="ri-user-fill absolute"></i>
+                                    </div>
+                                    <h1 className='font-semibold text-lg'>{user.email}</h1>
+                                </div>
+                            )
+
+
+                        })}
                     </div>
                 </div>
             </section>
@@ -82,15 +136,17 @@ const Project = () => {
                         </header>
                         <div className="users-list flex flex-col gap-2 mb-16 max-h-96 overflow-auto">
                             {users.map(user => (
-                                <div key={user.id} className={`user cursor-pointer hover:bg-slate-200 ${selectedUserId.indexOf(user.id) != -1 ? 'bg-slate-200' : ""} p-2 flex gap-2 items-center`} onClick={() => handleUserClick(user.id)}>
+                                <div key={user.id} className={`user cursor-pointer hover:bg-slate-200 ${Array.from(selectedUserId).indexOf(user._id) != -1 ? 'bg-slate-200' : ""} p-2 flex gap-2 items-center`} onClick={() => handleUserClick(user._id)}>
                                     <div className='aspect-square relative rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'>
                                         <i className="ri-user-fill absolute"></i>
                                     </div>
-                                    <h1 className='font-semibold text-lg'>{user.name}</h1>
+                                    <h1 className='font-semibold text-lg'>{user.email}</h1>
                                 </div>
                             ))}
                         </div>
-                        <button className='absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-blue-600 text-white rounded-md'>
+                        <button
+                            onClick={addCollaborators}
+                            className='absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-blue-600 text-white rounded-md'>
                             Add Collaborators
                         </button>
                     </div>
