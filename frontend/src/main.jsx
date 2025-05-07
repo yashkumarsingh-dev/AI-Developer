472373cd4c2708b732fe4c19b69a7dff42cb40f3
cooldude-69 <bca45027.20@bitmesra.ac.in>
2025-05-07 22:29:07 +0530

    fourt commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import "remixicon/fonts/remixicon.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </React.StrictMode>
-  );
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
